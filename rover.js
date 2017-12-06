d4f3f01a353eb771291c940866c879e412e1daed
g-suraj <sg6215@ic.ac.uk>
2017-12-06 17:18:58 +0000

    More test cases for instantiation of the rover

--- rover.js
+++ rover.js
@@ -1,8 +1,12 @@
 let assert = require('assert')
 let Rover = class {
+  // We assume the grid is 100 x 100?
   constructor (x, y, compass) {
-    assert(x !== undefined)
-    assert(y !== undefined)
+    assert(typeof x === 'number')
+    assert(typeof y === 'number')
+    assert(typeof y === 'string')
+    assert(x !== undefined && x >= 0 && x < 100)
+    assert(y !== undefined && y >= 0 && y < 100)
     assert(compass !== undefined)
     this.x = x
     this.y = y
--- test/rover.test.js
+++ test/rover.test.js
@@ -1,14 +1,50 @@
 let Rover = require('../rover')
 let assert = require('assert')
-describe('Rover Instance', function () {
-  it('Should not have any undefined members', function () {
+describe('Rover Instance', () => {
+  it('Should not have any undefined members', () => {
     assert.throws(() => {
       let rover = new Rover()
     }, Error)
   })
-  it('Should not have any partially undefined members', function () {
+  it('Should not have any partially undefined members', () => {
     assert.throws(() => {
       let rover = new Rover(1, 4)
     }, Error)
+    assert.throws(() => {
+      let rover = new Rover(4)
+    }, Error)
+  })
+  describe('after successful instantiation', () => {
+    it('Should not have any directional members ill defined', () => {
+      assert.throws(() => {
+        let rover = new Rover(101, 4, 'N')
+      }, Error)
+      assert.throws(() => {
+        let rover = new Rover(-1, 4, 'N')
+      }, Error)
+      assert.throws(() => {
+        let rover = new Rover(10, 400, 'N')
+      }, Error)
+      assert.throws(() => {
+        let rover = new Rover(10, 10400, 'N')
+      }, Error)
+      assert.throws(() => {
+        let rover = new Rover('nope', 10400, 'N')
+      }, Error)
+      assert.throws(() => {
+        let rover = new Rover(1, 'something', 'N')
+      }, Error)
+      assert.throws(() => {
+        let rover = new Rover('something', 1, 'N')
+      }, Error)
+      assert.throws(() => {
+        let rover = new Rover('something', 'something', 'N')
+      }, Error)
+    })
+    it('Should not have any compass members ill defined', () => {
+      assert.throws(() => {
+        let rover = new Rover(1, 1, 0)
+      }, Error)
+    })
   })
 })
